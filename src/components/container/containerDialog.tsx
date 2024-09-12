81edfa325b4f611bbb1ce50e77cfb229854a0f9c
Fernando Cezar Vicari <fcvicari@gmail.com>
2024-09-11 21:10:42 -0300

    Changed component container margins.

--- src/components/container/containerDialog.tsx
+++ src/components/container/containerDialog.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 export function ContainerDialog({ children }: ContainerProps) {
   return (
-    <div className="flex flex-col gap-4 bg-primary-foreground text-black border-[1px] shadow-md rounded-lg p-4 min-w-96 my-3 max-w-5xl">
+    <div className="flex flex-col gap-4 bg-primary-foreground text-black border-[1px] shadow-md rounded-lg px-4 md:px-6 py-2 min-w-96 my-3 max-w-5xl">
       { children }
     </div>
   )
